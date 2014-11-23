a7d59151cb70a53052e48a85a31ecae2206a945c
macartuche <macartuche@gmail.com>
2014-11-23 13:38:46 -0500

    agregando usuarios a la tabla usuario

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,41 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		 $this->call('UserTableSeeder');
 	}
+
+}
+
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('usuario')->delete();
+
+        $usuarios = array(
+        	 
+		        	array(
+		        		'nombres' => 'Daniel Pechán',
+		        		'apellidos' => 'León Ortega',
+		        		'mail' => 'danielPechan@gmail.com' ,
+		        		'password' => Hash::make('admin')
+		        	),
+		        	array(
+		        		'nombres' => 'Ramiro Josimar',
+		        		'apellidos' => 'Jiménez Jiménez',
+		        		'mail' => 'ramirojosimar@gmail.com' ,
+		        		'password' => Hash::make('admin')
+		        	),
+		        	array(
+		        		'nombres' => 'Manuel Alberto',
+		        		'apellidos' => 'Cartuche Flores',
+		        		'mail' => 'macartuche@gmail.com' ,
+		        		'password' => Hash::make('admin')
+		        	)
+        	);
+
+		DB::table('usuario')->insert( $usuarios );
+    }
 
 }
