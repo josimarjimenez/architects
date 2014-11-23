893177bcf188b04c25dfe5951936ef5af66b3bee
macartuche <macartuche@gmail.com>
2014-11-23 13:17:37 -0500

    Tabla de usuarios

---
+++ app/database/migrations/2014_11_23_175112_create-users-table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+	
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('usuario', function(Blueprint $table)
+       {
+			$table->increments('id');
+			$table->string('nombres', 100);
+			$table->string('apellidos', 100);
+			$table->string('mail', 50)->unique();
+			$table->string('password', 64);
+			$table->string('direccion', 100);
+			$table->string('avatar', 100);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		 Schema::drop('usuarios');
+	}
+
+}
