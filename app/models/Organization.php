094c296db6770bf81335f0b04b141bafd291b928
macartuche <macartuche@gmail.com>
2014-12-05 13:35:08 -0500

    grabando organizacion

--- app/models/Organization.php
+++ app/models/Organization.php
@@ -1,9 +1,19 @@
 <?php 
 
-class Organization extends Eloquent {
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+class Organization extends Eloquent implements UserInterface, RemindableInterface{
 	protected $table = 'organization';
 
-	//relationSHIP
+	use UserTrait, RemindableTrait;
+	public static $rules = array(
+	    'name'=>'required|alpha_num|min:2',
+	    'test'=>'required|alpha|min:2'
+    );
+	
+
 	public function projects() {
 		return $this->hasMany('Project','organizationid');
 	}
