ac9973c1c4bd453361f4a1038814235cf65bdf95
dlo85 <danileonortega0585@gmail.com>
2014-12-16 18:37:01 -0500

    guardar

---
+++ app/views/layouts/rol/rolForm.blade.php
@@ -0,0 +1,17 @@
+<div id="selectRol">
+
+ 	<div class="panel panel-default">
+
+		{{ Form::open(array('url'=>'rol/ingresar', 'class'=>'form-signup')) }}
+			{{ Form::label('name', 'Nombre') }}
+			{{ Form::text('name', null, array('class'=>'input-block-level', 'placeholder'=>'Nombre del Rol')) }}
+			{{ Form::label('description', 'Descripcion') }}
+			{{ Form::text('description', null, array('class'=>'input-block-level', 'placeholder'=>'Descripcion del Rol')) }}
+ 			
+ 			{{ Form::submit('Guardar', array('class'=>'btn btn-large btn-primary btn-block'))}}
+
+ 		{{ Form::close() }}
+
+ 	</div>
+
+ </div>
