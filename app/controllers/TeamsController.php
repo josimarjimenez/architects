07e080b15c8e1b21b402a28b6686ea00900f3876
dlo85 <danileonortega0585@gmail.com>
2014-12-16 23:27:23 -0500

    subir

--- app/controllers/TeamsController.php
+++ app/controllers/TeamsController.php
@@ -3,13 +3,19 @@
 class TeamsController extends BaseController {
 	protected $layout = "layouts.main";
 
-	public function getCreate(){  
-		$this->layout->content = View::make('layouts.teams.teamsForm')
-		->with('organization', app('organization'))  ;
+ 	public function index() {}
+
+    public function show($id) {}
+
+
+	public function create(){  
+		$this->layout->content = View::make('layouts.teams.new')
+		->with('organization', app('organization')) 
+		->with('type',  'new') ;
 	}
 
 	//save mew
-	public function postCreate(){
+	public function store(){
 		$validator = Validator::make(Input::all(), Teams::$rules);
 
 		if ($validator->passes()) { 
@@ -19,30 +25,49 @@ class TeamsController extends BaseController {
 			$teams->save();
 
 			$organization = app('organization');
-			return Redirect::to('organization/name/'.$organization->auxName.'/teams')
-			->with('message', 'Equipo creado con éxito'); 
+			return Redirect::to('organization/name/'.$organization->auxName.'/projects')
+			->with('message', 'Registro creado con exito'); 
 		}else{
-			return Redirect::to('teams/create')
+			return Redirect::to('projects/create')
 			->with('message', 'Ocurrieron los siguientes errores')
 			->withErrors($validator)
 			->withInput();   	
 		}
 	}
 
-	public function getEdit($id){
-		 
-		$teams = Teams::findOrFail($id);
-		$organization = app('organization');
-		if($teams==null){
-			return Redirect::to('/organization/name/'.$organization->auxName.'/teams')
-			->with('message', 'No hay el Equipo');
+	public function edit($id){
+		try {
+			$project = Project::findOrFail($id);
+		}catch (Illuminate\Database\Eloquent\ModelNotFoundException $e) { 
+			$organization = app('organization');
+		    return Redirect::to('/organization/name/'.$organization->auxName.'/projects')
+			->with('message', 'No existe el proyecto');
 		}
-		$this->layout->content = View::make('layouts.teams.teamsEditForm')
-		->with('teams', $teams)  ;
+		
+		 
+		$this->layout->content = View::make('layouts.projects.new')
+		->with('project', $project)->with('type',  'edit')  ;
 	}
 
-	public function postUpdate(){
-		
+	public function update($id){
+		$project = Project::findOrFail($id);
+		$project->fill(Input::all());
+		$project->save();
+		$organization = app('organization');
+		return Redirect::to('organization/name/'.$organization->auxName.'/projects')
+			->with('message', 'Registro actualizado');
+	}
+
+
+	public function destroy($id){
+		//project
+		$project = Project::find($id);
+		if( sizeof($project->iterations) < 1 ){
+			$project->delete();
+		}
+
+		$organization = app('organization');
+		return Redirect::to('organization/name/'.$organization->auxName.'/projects')->with('message', 'Registro eliminado');
 	}
 }
 ?>
