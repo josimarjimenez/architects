60eaccce14de0447a8dee0e3d6a076ce75303e9c
macartuche <macartuche@gmail.com>
2014-12-17 01:47:36 -0500

    eliminando multiempresa

--- app/views/layouts/users/dashboard.blade.php
+++ app/views/layouts/users/dashboard.blade.php
@@ -90,29 +90,4 @@
 		</ul>
 	</div>
 </div>
-
-<div id="selectOrganization">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3 class="panel-title">Seleccione la organización</h3>
-		</div>
-		<div class="panel-body">
-			<ul>
-				<li>
-					<h3>
-						{{ HTML::linkAction('OrganizationsController@getIndex', 'Mi empresa 1')}}
-					</h3>
-					<div>8 Projectos - 2 Equipos</div>
-				</li>
-				<li>
-					<h3><a href="">Mi empresa 2</a></h3>
-					<div>2 Projectos - 1 Equipo(s)</div>
-				</li>
-			</ul>
-			{{ HTML::linkAction('OrganizationsController@getNew', 'Crear nueva organización', 
-				array(), 
-				array('class'=>'btn btn-success'))}}
-			</div>
-		</div>
-	</div>
-
+ <br>
