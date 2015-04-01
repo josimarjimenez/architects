140c8ecbf57679dd241e1432b283840003e40e87
Daniel <danileonortega0585@gmail.com>
2015-03-31 21:45:36 -0500

    bajar

--- tests/functional/TestCept.php
+++ tests/functional/TestCept.php
@@ -1,6 +1,9 @@
 <?php 
 $I = new FunctionalTester($scenario);
 $I->wantTo('Ingreso al Sistema');
+$I->amLoggedAs('Usuario' => 'danielPechan@gmail.com', 'password' => 'admin');
 $I->amOnPage('/users/login');
-$I->amLoggedAs('Usuario'=>'danielPechan@gmail.com', );
-?>
+$I->click('Ingresar');
+$I->seeCurrentEquals('/users/dashboard');
+$I->see('Unesco');
+?>	
