75a31cda68eeb8d2212285031aebe133aa0f17dc
Daniel <danileonortega0585@gmail.com>
2015-04-09 22:16:57 -0500

    bajar

--- tests/acceptance/SigninCept.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('Ingreso al Sistema');
-$I->amOnPage('/users/login');
-$I->fillField('Usuario','danielPechan@gmail.com');
-$I->fillField('password','admin');
-$I->click('Ingresar');
-$I->see('Unesco');
-?>
--- tests/functional/TestCept.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$I = new FunctionalTester($scenario);
-$I->wantTo('Ingreso al Sistema');
-$I->amLoggedAs('Usuario' => 'danielPechan@gmail.com', 'password' => 'admin');
-$I->amOnPage('/users/login');
-$I->click('Ingresar');
-$I->seeCurrentEquals('/users/dashboard');
-$I->see('Unesco');
-?>	
