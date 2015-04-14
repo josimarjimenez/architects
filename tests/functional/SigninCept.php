bac2985120328e1bc721780689bcb1b15d7afa8d
Daniel <danileonortega0585@gmail.com>
2015-04-13 21:44:31 -0500

    bajar

--- tests/functional/SigninCept.php
+++ tests/functional/SigninCept.php
@@ -1,11 +1,4 @@
 <?php 
 $I = new FunctionalTester($scenario);
 $I->wantTo('Ingresar al Sistema');
-$I->amOnPage('/');
-$I->click('login');
-$I->fillField('Usuario', 'danielPechan@gmail.com');
-$I->fillField('Clave', 'admin');
-$I->click('Ingresar');
-$I->see('Unesco', 'Ha iniciado sesión');
-$I->see('Ha iniciado sesión');
 ?>
