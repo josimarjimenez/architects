129b30e811b15373961b33cc8f067c1ff5b8bf72
josimar <josimar.revolution@gmail.com>
2015-04-26 22:01:07 -0500

    Se agrego sistema para envio de notificaciones o mensajes

--- app/views/layouts/menu.blade.php
+++ app/views/layouts/menu.blade.php
@@ -15,8 +15,14 @@
 		<div style="clear:both"></div> 
 		<div class="col_6">
 			<ul class="project-menu-horizontal-list">
-				<li><a href="edit/{{ Auth::id(); }}"><i class="topmenu-icon icon-glyph icon-group"></i> Mi perfil </a></li>
-				<li>{{ HTML::link('users/logout', 'Cerrar sesión',array('class'=>'drop megamenu-top-header')) }}</li>
+				<li>
+					<!--<a href="edit/{{ Auth::id(); }}"><i class="topmenu-icon icon-glyph icon-group"></i>Mi perfil</a> 
+					<a href="users/editProfile"><i class="topmenu-icon icon-glyph icon-group"></i>Mi perfil</a> -->
+					{{ HTML::link('users/editprofile', 'Mi perfil',array('class'=>'drop megamenu-top-header')) }}
+				</li>
+				<li>
+					{{ HTML::link('users/logout', 'Cerrar sesión',array('class'=>'drop megamenu-top-header')) }}
+				</li>
 			</ul>
 		</div> 
 	</div> 
