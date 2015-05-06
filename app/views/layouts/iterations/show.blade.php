38dd09a9db6989aa453fd5d45bb0934d9769a96e
josimar <josimar.revolution@gmail.com>
2015-05-05 23:36:13 -0500

    ssss

--- app/views/layouts/iterations/show.blade.php
+++ app/views/layouts/iterations/show.blade.php
@@ -1,4 +1,4 @@
- 
+
 @if(!empty($message))
 <ul id="messages">
 	<li id="message_1"><a onclick="$('#message_1').fadeOut(); return false;" href="#"><small>Limpiar</small></a>{{ $message }}</li>
@@ -40,15 +40,6 @@
 
 			<img src="{{ action('GraphicsController@iterationSummary', array('iteration' =>  $iteration->id  )) }}">
 
-			<img src="{{ action('GraphicsController@iterationSummary', array('iteration' =>  $iteration->id  )) }}">
-			
-			<img src="{{ action('GraphicsSummaryController@summary', array('iteration' =>  $iteration->id  )) }}">
-
-			<img src="{{ action('GraphicsTestController@summary', array('iteration' =>  $iteration->id  )) }}">
-
-			<img src="{{ action('GraphicsSpendingController@summary', array('iteration' =>  $iteration->id  )) }}">
- 
-			<img src="{{ action('GraphicsTaskController@bar_task', array('iteration' =>  $iteration->id)) }}"> 
 		@else
 			<!--<img src="https://d11uy15xvlvge3.cloudfront.net/static/v105/scrumdo/images/burndown.png"> -->
 			<img src="{{ action('GraphicsTaskController@bar_task', array('iteration' =>  $iteration->id)) }}">
